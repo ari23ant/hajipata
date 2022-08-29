d2b87d43913f5e15363a0106086b106bff826bb8
ari23 <ari23ant@gmail.com>
2022-08-30 08:52:01 +0900

    [add] 重み初期化とシグモイド関数

--- src/6_4dashlogisticreqression.jl
+++ src/6_4dashlogisticreqression.jl
@@ -3,6 +3,7 @@ using RDatasets
 using DataFrames
 using Statistics
 using LinearAlgebra
+using Random
 
 data = Dict{Symbol, Any}()
 data[:iris] = dataset("datasets", "iris")
@@ -20,13 +21,34 @@ function process(data::Dict{Symbol, Any}, rslt::Dict{Symbol, Any})
     #plot_setosa_versicolor_virginica(data[:setosa], data[:versicolor], data[:virginica], "SepalLength", "SepalWidth", title="Iris")
 
     # 分類するデータを整理
-    #X1, X2 = :PetalLength, :PetalWidth
-    #pos, neg = :setosa, :versicolor
-    dfpos, dfneg = maketargetdataframe(data[:iris], :setosa, :versicolor, :PetalLength, :PetalWidth)
-    myprint(dfpos, dfneg)
-    rslt[:dfpos], rslt[:dfneg] = dfpos, dfneg
+    X1, X2 = :PetalLength, :PetalWidth
+    pos, neg = :setosa, :versicolor
+    dftgt = maketargetdataframe(data[:iris], pos, neg, X1, X2)
+    dfpos = dftgt[dftgt.class .== 1, :]
+    dfneg = dftgt[dftgt.class .== 0, :]
+
+    rslt[:dftgt], rslt[:dfpos], rslt[:dfneg] = dftgt, dfpos, dfneg
+
+    # 重みの初期化
+    seed = 23
+    weight = setweight(3, seed)
+    myprint(weight)
+
+    myprint(sigmoid(Vector(dftgt[1, 1:2]), weight))
 
     println("Done")
+end
+
+function sigmoid(x::Vector, w::Vector)
+    a = w[1:2]' * x + w[3]
+    f = 1.0 / (1.0 + exp(-a))
+    return f
+end
+
+function setweight(size::Int, seed::Int)
+    Random.seed!(seed)
+    weight = rand(size)
+    return weight
 end
 
 function maketargetdataframe(df::DataFrame, pos::Symbol, neg::Symbol, X1::Symbol, X2::Symbol)
@@ -36,7 +58,10 @@ function maketargetdataframe(df::DataFrame, pos::Symbol, neg::Symbol, X1::Symbol
     dfneg = df[df.Species .== string(neg), [X1, X2]]
     dfneg[!, :class] .= 0
 
-    return dfpos, dfneg
+    dftgt = vcat(dfpos, dfneg)
+
+    #return dftgt, dfpos, dfneg
+    return dftgt
 end
 
 function plot_setosa_versicolor_virginica(setosa::DataFrame, versicolor::DataFrame, virginica::DataFrame, xlabel::String, ylabel::String; xlim::Any=nothing, ylim::Any=nothing, title::String="")::Figure
